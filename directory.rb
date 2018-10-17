d752787e99d18f58773b7c751865ceaeb6343ee0
Dana Gajewska <mdgajewska@outlook.com>
2018-10-17 11:35:14 +0100

    Print by cohort, Add no entry output

--- directory.rb
+++ directory.rb
@@ -1,4 +1,7 @@
 def input_students
+  months = ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"]
+
   puts 'Please enter the names of the students'
   puts 'To finish, just hit return twice'
 
@@ -7,7 +10,12 @@ def input_students
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
+
+    puts 'Which cohort is this student in?'
+    cohort = gets.chomp.capitalize
+
+    months.include?(cohort) ? students << {name: name, cohort: cohort.to_sym} : next
+
     puts "Now we have #{students.count} students"
 
     name = gets.chomp
@@ -16,22 +24,34 @@ def input_students
   students
 end
 
-def print_header
-  puts 'The students of Villains Academy'
-  puts '-------------'
+def print_header(students)
+  if students.length > 0
+    puts 'The students of Villains Academy'
+    puts '-------------'
+  end
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.map{ |student| student[:cohort] }.uniq.each do |cohort|
+    puts cohort
+      students.each do |student|
+          puts student[:name] if student[:cohort] == cohort
+      end
+    puts ""
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  if names.count > 1
+    puts "Overall, we have #{names.count} great students"
+  elsif names.count == 1
+    puts 'Overall, we have 1 great student'
+  else
+    puts 'We have no students'
+  end
 end
 
 students = input_students
-print_header
+print_header(students)
 print(students)
 print_footer(students)
