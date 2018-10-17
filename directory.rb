919a5f6238152c5b37ab65ead97a8dd1e8a5550b
Dana Gajewska <mdgajewska@outlook.com>
2018-10-17 13:53:19 +0100

    Refactor into separate methods

--- directory.rb
+++ directory.rb
@@ -1,35 +1,38 @@
+@months = ["January","February","March","April","May","June","July",
+          "August","September","October","November","December"]
+
+@students = []
+
 def interactive_menu
-  students = []
   while true do
-    puts '1. Input students'
-    puts '2. List students'
-    puts '9. Exit'
+    print_menu
+    process(gets.chomp)
+  end
+end
 
-    selection = gets.chomp
+def print_menu
+  puts '1. Input students'
+  puts '2. List students'
+  puts '9. Exit'
+end
 
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header(students)
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    list_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
   end
 end
 
 def input_students
-  months = ["January","February","March","April","May","June","July",
-            "August","September","October","November","December"]
 
   puts 'Please enter the names of the students'
   puts 'To finish, just hit return twice'
-
-  students = []
 
   name = gets.chomp
 
@@ -38,37 +41,42 @@ def input_students
     puts 'Which cohort is this student in?'
     cohort = gets.chomp.capitalize
 
-    months.include?(cohort) ? students << {name: name, cohort: cohort.to_sym} : next
+    @months.include?(cohort) ? @students << {name: name, cohort: cohort.to_sym} : next
 
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
 
     name = gets.chomp
   end
 
-  students
 end
 
-def print_header(students)
-  if students.length > 0
+def list_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def print_header
+  if @students.length > 0
     puts 'The students of Villains Academy'
     puts '-------------'
   end
 end
 
-def print(students)
-  students.map{ |student| student[:cohort] }.uniq.each do |cohort|
+def print_student_list
+  @students.map{ |student| student[:cohort] }.uniq.each do |cohort|
     puts cohort
-      students.each do |student|
+      @students.each do |student|
           puts student[:name] if student[:cohort] == cohort
       end
     puts ""
   end
 end
 
-def print_footer(names)
-  if names.count > 1
-    puts "Overall, we have #{names.count} great students"
-  elsif names.count == 1
+def print_footer
+  if @students.count > 1
+    puts "Overall, we have #{@students.count} great students"
+  elsif @students.count == 1
     puts 'Overall, we have 1 great student'
   else
     puts 'We have no students'
